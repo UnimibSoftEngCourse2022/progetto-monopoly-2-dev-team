a3bb7e874e83a337355fc838d5b9bc8e78c5bb42
CPozzoli7 <c.pozzoli7@campus.unimib.it>
2022-01-10 00:36:46 +0100

    Refactor for ColoredPriceManager

--- src/main/java/manager/pricemanager/ColoredPriceManager.java
+++ src/main/java/manager/pricemanager/ColoredPriceManager.java
@@ -6,8 +6,6 @@ import model.player.PlayerModel;
 import model.property.PropertyModel;
 
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
 
 public class ColoredPriceManager extends PriceManager {
 
@@ -29,23 +27,24 @@ public class ColoredPriceManager extends PriceManager {
 
     private int getCleanRent(PropertyModel property) {
         int rent;
-        if (property.getHouseNumber() == 0) {
-            if (property.getHotelNumber() == 0) {
-                rent = property.getBaseRent();
-                PlayerModel player = propertyOwnerMapper.getOwner(property);
-                if (player != null) {
-                    List<PropertyModel> categoryProperties = propertyCategoryMapper
-                            .getCategoryProperties(property.getCategory());
-                    boolean sameOwner = true;
-                    for (int i = 0; i < categoryProperties.size() && sameOwner; i++) {
-                        sameOwner = player.equals(propertyOwnerMapper.getOwner(categoryProperties.get(i)));
-                    }
-                }
-            } else {
-                rent = property.getHotelRent();
-            }
-        } else {
+        if (property.getHotelNumber() != 0) {
+            rent = property.getHotelRent();
+        } else if (property.getHouseNumber() != 0) {
             rent = property.getHouseRent();
+        } else {
+            rent = property.getBaseRent();
+            PlayerModel player = propertyOwnerMapper.getOwner(property);
+            if (player != null) {
+                List<PropertyModel> categoryProperties = propertyCategoryMapper
+                        .getCategoryProperties(property.getCategory());
+                boolean sameOwner = true;
+                for (int i = 0; i < categoryProperties.size() && sameOwner; i++) {
+                    sameOwner = player.equals(propertyOwnerMapper.getOwner(categoryProperties.get(i)));
+                }
+                if(sameOwner) {
+                    rent *= 2;
+                }
+            }
         }
         return rent;
     }
