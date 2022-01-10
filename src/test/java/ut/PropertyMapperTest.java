54eea23ba665ee07a61ddc60451f6533cef21bef
Emanuele Papa <e.papa6@campus.unimib.it>
2022-01-10 14:39:34 +0100

    Updated PropertyMapperTest

--- src/test/java/ut/PropertyMapperTest.java
+++ src/test/java/ut/PropertyMapperTest.java
@@ -29,8 +29,30 @@ public class PropertyMapperTest {
 
         PropertyModel model = categoryProperties.get(0);
 
+
         Assert.assertEquals(PropertyCategory.BROWN, model.getCategory());
         Assert.assertTrue(model.getHouseRent(3) < model.getHouseRent(4));
+        Assert.assertEquals("Mediterranean Avenue", model.getName());
+        Assert.assertEquals(60, model.getPrice());
+        Assert.assertEquals(50, model.getHousePrice());
+        Assert.assertEquals(50, model.getHotelPrice());
+        Assert.assertEquals(30, model.getMortgageValue());
+
+
+        Assert.assertEquals(250, model.getHotelRent());
+        Assert.assertTrue(model.isImprovable());
+        model.setHouseNumber(1);
+        Assert.assertEquals(1, model.getHouseNumber());
+        Assert.assertEquals(10, model.getHouseRent());
+        model.setHouseNumber(4);
+        Assert.assertEquals(4, model.getHouseNumber());
+        Assert.assertEquals(160, model.getHouseRent());
+        /*
+        model.setHotelNumber(1);
+        Assert.assertEquals(0, model.getHouseNumber());
+        Assert.assertEquals(1, model.getHotelNumber());
+        Assert.assertEquals(250, model.getHotelNumber());
+         */
     }
 
 }
