7f9ffa79a0280928b1b67ae3bb1d44dc65cf58c0
CPozzoli7 <c.pozzoli7@campus.unimib.it>
2022-01-11 00:36:05 +0100

    Refactor for dice class

---
+++ src/main/java/controller/DiceRoller.java
@@ -0,0 +1,7 @@
+package controller;
+
+import util.Pair;
+
+public interface DiceRoller {
+    Pair<Integer, Integer> rollDice();
+}
--- src/main/java/util/Dice.java
+++ src/main/java/util/Dice.java
@@ -1,11 +1,9 @@
-import util.Pair;
+package util;
 
-public class DiceRoller {
-
-    public Pair<Integer, Integer> roll() {
+public class Dice {
+    public static Pair<Integer, Integer> roll() {
         int first = (int) (Math.random() * 6) + 1;
         int second = (int) (Math.random() * 6) + 1;
         return new Pair<>(first, second);
     }
-
 }
