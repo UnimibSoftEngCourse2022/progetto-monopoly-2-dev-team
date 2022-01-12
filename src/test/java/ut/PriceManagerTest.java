ce89aef12de0afc660d050aea7a323ad96a52de6
CPozzoli7 <c.pozzoli7@campus.unimib.it>
2022-01-12 18:09:58 +0100

    Added tests for PriceManagerDispatcher

--- src/test/java/ut/PriceManagerTest.java
+++ src/test/java/ut/PriceManagerTest.java
@@ -1,10 +1,7 @@
 package ut;
 
 import controller.DiceRoller;
-import manager.pricemanager.ColoredPriceManager;
-import manager.pricemanager.PriceManager;
-import manager.pricemanager.RailroadPriceManager;
-import manager.pricemanager.UtilityPriceManager;
+import manager.pricemanager.*;
 import model.PropertyCategoryMapper;
 import model.PropertyOwnerMapper;
 import model.player.PlayerModel;
@@ -69,5 +66,51 @@ public class PriceManagerTest {
             propertyOwnerMapper.setOwner(categoryProperty, playerModel);
         }
         Assert.assertEquals(10 * (5 + 3), priceManager.getRent(property));
+    }
+
+    @Test
+    public void PriceManagerDispatcherNoRandomizationTest() {
+        PriceManager priceManager = new PriceManagerDispatcher(
+                propertyOwnerMapper,
+                propertyCategoryMapper,
+                () -> new Pair<>(5, 3)
+        );
+        PropertyModel property = TestUtils
+                .getPropertyCategoryMapper()
+                .getCategoryProperties(PropertyCategory.UTILITY)
+                .get(0);
+        Assert.assertEquals(150, priceManager.getPrice(property));
+        Assert.assertEquals(4 * (5 + 3), priceManager.getRent(property));
+        PlayerModel playerModel = new PlayerModel("0", "name", 1000);
+        for (PropertyModel categoryProperty : propertyCategoryMapper.getCategoryProperties(property.getCategory())) {
+            propertyOwnerMapper.setOwner(categoryProperty, playerModel);
+        }
+        Assert.assertEquals(10 * (5 + 3), priceManager.getRent(property));
+
+        property = TestUtils
+                .getPropertyCategoryMapper()
+                .getCategoryProperties(PropertyCategory.RAILROAD)
+                .get(0);
+        Assert.assertEquals(200, priceManager.getPrice(property));
+        Assert.assertEquals(25, priceManager.getRent(property));
+        for (PropertyModel categoryProperty : propertyCategoryMapper.getCategoryProperties(property.getCategory())) {
+            propertyOwnerMapper.setOwner(categoryProperty, playerModel);
+        }
+        Assert.assertEquals(200, priceManager.getRent(property));
+
+        property = TestUtils
+                .getPropertyCategoryMapper()
+                .getCategoryProperties(PropertyCategory.ORANGE)
+                .get(0);
+        Assert.assertEquals(180, priceManager.getPrice(property));
+        Assert.assertEquals(14, priceManager.getRent(property));
+        for (PropertyModel categoryProperty : propertyCategoryMapper.getCategoryProperties(property.getCategory())) {
+            propertyOwnerMapper.setOwner(categoryProperty, playerModel);
+        }
+        Assert.assertEquals(28, priceManager.getRent(property));
+        property.setHouseNumber(2);
+        Assert.assertEquals(200, priceManager.getRent(property));
+        property.setHotelNumber(1);
+        Assert.assertEquals(950, priceManager.getRent(property));
     }
 }
