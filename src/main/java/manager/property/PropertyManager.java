5e609ebcc175e037577ad6757ccf7ebc9fc6e3bb
CPozzoli7 <c.pozzoli7@campus.unimib.it>
2022-01-13 22:41:09 +0100

    Refactor for PropertyManager

--- src/main/java/manager/property/PropertyManager.java
+++ src/main/java/manager/property/PropertyManager.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class PropertyManager {
+    private final PropertyModel property;
     private PropertyOwnerMapper ownerMapper;
     private PropertyCategoryMapper categoryMapper;
     private PriceManager priceManager;
@@ -18,14 +19,15 @@ public class PropertyManager {
     private static final int MAX_NUMBER_OF_HOUSES = 4;
     private static final int MAX_NUMBER_OF_HOTEL = 1;
 
-    public PropertyManager(PropertyOwnerMapper ownerMapper, PropertyCategoryMapper categoryMapper, PriceManager priceManager) {
+    public PropertyManager(PropertyModel property, PropertyOwnerMapper ownerMapper, PropertyCategoryMapper categoryMapper, PriceManager priceManager) {
+        this.property = property;
         this.ownerMapper = ownerMapper;
         this.categoryMapper = categoryMapper;
         this.priceManager = priceManager;
     }
 
-    public boolean buildHouse(PropertyModel property) {
-        if (property != null && canImproveHouse(property)) {
+    public boolean buildHouse() {
+        if (property != null && canImproveHouse()) {
             property.setHouseNumber(
                     property.getHouseNumber() + 1
             );
@@ -34,8 +36,8 @@ public class PropertyManager {
         return false;
     }
 
-    public boolean buildHotel(PropertyModel property) {
-        if (property != null && canImproveHotel(property)) {
+    public boolean buildHotel() {
+        if (property != null && canImproveHotel()) {
             property.setHotelNumber(1);
             property.setHouseNumber(0);
             return true;
@@ -43,8 +45,8 @@ public class PropertyManager {
         return false;
     }
 
-    public boolean removeHouse(PropertyModel property) {
-        if (property != null && canRemoveHouse(property)) {
+    public boolean removeHouse() {
+        if (property != null && canRemoveHouse()) {
             property.setHouseNumber(
                     property.getHouseNumber() - 1
             );
@@ -53,8 +55,8 @@ public class PropertyManager {
         return false;
     }
 
-    public boolean removeHotel(PropertyModel property) {
-        if (property != null && canRemoveHotel(property)) {
+    public boolean removeHotel() {
+        if (property != null && canRemoveHotel()) {
             property.setHotelNumber(0);
             property.setHouseNumber(4);
             return true;
@@ -62,77 +64,76 @@ public class PropertyManager {
         return false;
     }
 
-    public boolean mortgage(PropertyModel property) {
-        if (!canMortgage(property)) {
+    public boolean mortgage() {
+        if (!canMortgage()) {
             return false;
         }
         property.setMortgaged(true);
         return true;
     }
 
-    public boolean liftMortgage(PropertyModel property) {
-        if (!canLiftMortgage(property)) {
+    public boolean liftMortgage() {
+        if (!canLiftMortgage()) {
             return false;
         }
         property.setMortgaged(false);
         return true;
     }
 
-    public boolean canImproveHouse(PropertyModel property) {
+    public boolean canImproveHouse() {
         return property != null &&
-                hasRightCategory(property) &&
-                hasValidBuildingsForHouse(property) &&
-                ownerHasEveryPropertyInCategoryAndUnmortgaged(property) &&
-                hasLessOrEqualBuildingsInCategoryForBuildingHouse(property);
+                hasRightCategory() &&
+                hasValidBuildingsForHouse() &&
+                ownerHasEveryPropertyInCategoryAndUnmortgaged() &&
+                hasLessOrEqualBuildingsInCategoryForBuildingHouse();
     }
 
-    public boolean canImproveHotel(PropertyModel property) {
+    public boolean canImproveHotel() {
         return property != null &&
-                hasRightCategory(property) &&
-                hasValidBuildingsForHotel(property) &&
-                ownerHasEveryPropertyInCategoryAndUnmortgaged(property) &&
-                hasLessOrEqualBuildingsInCategoryForBuildingHotel(property);
+                hasRightCategory() &&
+                hasValidBuildingsForHotel() &&
+                ownerHasEveryPropertyInCategoryAndUnmortgaged() &&
+                hasLessOrEqualBuildingsInCategoryForBuildingHotel();
     }
 
-    public boolean canRemoveHouse(PropertyModel property) {
+    public boolean canRemoveHouse() {
         return property != null &&
-                hasRightCategory(property) &&
-                hasValidBuildingsForRemovingHouse(property) &&
-                hasLessOrEqualBuildingsInCategoryForRemovingHouse(property);
+                hasRightCategory() &&
+                hasValidBuildingsForRemovingHouse() &&
+                hasLessOrEqualBuildingsInCategoryForRemovingHouse();
     }
 
-    public boolean canRemoveHotel(PropertyModel property) {
-        return property != null &&
-                hasRightCategory(property) &&
-                hasValidBuildingsForRemovingHotel(property) &&
-                hasLessOrEqualBuildingsInCategoryForRemovingHotel(property);
+    public boolean canRemoveHotel() {
+        return hasRightCategory() &&
+                hasValidBuildingsForRemovingHotel() &&
+                hasLessOrEqualBuildingsInCategoryForRemovingHotel();
     }
 
-    public boolean canMortgage(PropertyModel property) {
-        return !property.isMortgaged() && canSell(property);
+    public boolean canMortgage() {
+        return !property.isMortgaged() && canSell();
     }
 
-    public boolean canSell(PropertyModel property) {
-        return property != null && zeroBuildingsInCategory(property);
+    public boolean canSell() {
+        return zeroBuildingsInCategory();
     }
 
-    public boolean canCollectRent(PropertyModel property) {
+    public boolean canCollectRent() {
         return !property.isMortgaged() && ownerMapper.getOwner(property) != null;
     }
 
-    public boolean canLiftMortgage(PropertyModel property) {
+    public boolean canLiftMortgage() {
         return property.isMortgaged();
     }
 
-    public void setOwner(PropertyModel property, PlayerModel player) {
+    public void setOwner(PlayerModel player) {
         ownerMapper.setOwner(property, player);
     }
 
-    public PlayerModel removeOwner(PropertyModel property) {
+    public PlayerModel removeOwner() {
         return ownerMapper.removeOwner(property);
     }
 
-    public PlayerModel getOwner(PropertyModel property) {
+    public PlayerModel getOwner() {
         return ownerMapper.getOwner(property);
     }
 
@@ -142,25 +143,26 @@ public class PropertyManager {
 
     //PRIVATE METHODS
 
-    private boolean hasRightCategory(PropertyModel property) {
+    private boolean hasRightCategory() {
         return property.isImprovable() && !property.isMortgaged();
     }
 
-    private boolean ownerHasEveryPropertyInCategoryAndUnmortgaged(PropertyModel property) {
+    private boolean ownerHasEveryPropertyInCategoryAndUnmortgaged() {
         List<PropertyModel> categoryProperties = categoryMapper
                 .getCategoryProperties(property.getCategory());
+        PlayerModel owner = ownerMapper.getOwner(property);
+        if(owner == null) {
+            return false;
+        }
         Stream<PropertyModel> ownerProperties = categoryProperties
                 .stream()
                 .filter(
-                        propertyModel -> {
-                            PlayerModel owner = ownerMapper.getOwner(property);
-                            return owner != null && !property.isMortgaged() && owner.equals(ownerMapper.getOwner(propertyModel));
-                        }
+                        propertyModel -> !property.isMortgaged() && owner.equals(ownerMapper.getOwner(propertyModel))
                 );
         return categoryProperties.size() == ownerProperties.count();
     }
 
-    private boolean zeroBuildingsInCategory(PropertyModel property) {
+    private boolean zeroBuildingsInCategory() {
         boolean valid = true;
         List<PropertyModel> categoryProperties = categoryMapper
                 .getCategoryProperties(property.getCategory());
@@ -170,15 +172,15 @@ public class PropertyManager {
         return valid;
     }
 
-    private boolean hasLessOrEqualBuildingsInCategoryForBuildingHouse(PropertyModel property) {
-        return hasLessOrEqualBuildingsInCategoryForHouse(property, false);
+    private boolean hasLessOrEqualBuildingsInCategoryForBuildingHouse() {
+        return hasLessOrEqualBuildingsInCategoryForHouse(false);
     }
 
-    private boolean hasLessOrEqualBuildingsInCategoryForRemovingHouse(PropertyModel property) {
-        return hasLessOrEqualBuildingsInCategoryForHouse(property, true);
+    private boolean hasLessOrEqualBuildingsInCategoryForRemovingHouse() {
+        return hasLessOrEqualBuildingsInCategoryForHouse(true);
     }
 
-    private boolean hasLessOrEqualBuildingsInCategoryForHouse(PropertyModel property, boolean removing) {
+    private boolean hasLessOrEqualBuildingsInCategoryForHouse(boolean removing) {
         boolean valid = true;
         List<PropertyModel> categoryProperties = categoryMapper
                 .getCategoryProperties(property.getCategory());
@@ -192,23 +194,23 @@ public class PropertyManager {
         return valid;
     }
 
-    private boolean hasValidBuildingsForHouse(PropertyModel property) {
+    private boolean hasValidBuildingsForHouse() {
         return property.getHotelNumber() == 0 && property.getHouseNumber() < MAX_NUMBER_OF_HOUSES;
     }
 
-    private boolean hasValidBuildingsForRemovingHouse(PropertyModel property) {
+    private boolean hasValidBuildingsForRemovingHouse() {
         return property.getHotelNumber() == 0 && property.getHouseNumber() > 0;
     }
 
-    private boolean hasLessOrEqualBuildingsInCategoryForBuildingHotel(PropertyModel property) {
-        return hasLessOrEqualBuildingsInCategoryForHotel(property, false);
+    private boolean hasLessOrEqualBuildingsInCategoryForBuildingHotel() {
+        return hasLessOrEqualBuildingsInCategoryForHotel(false);
     }
 
-    private boolean hasLessOrEqualBuildingsInCategoryForRemovingHotel(PropertyModel property) {
-        return hasLessOrEqualBuildingsInCategoryForHotel(property, true);
+    private boolean hasLessOrEqualBuildingsInCategoryForRemovingHotel() {
+        return hasLessOrEqualBuildingsInCategoryForHotel(true);
     }
 
-    private boolean hasLessOrEqualBuildingsInCategoryForHotel(PropertyModel property, boolean removing) {
+    private boolean hasLessOrEqualBuildingsInCategoryForHotel(boolean removing) {
         boolean valid = true;
         List<PropertyModel> categoryProperties = categoryMapper
                 .getCategoryProperties(property.getCategory());
@@ -224,11 +226,11 @@ public class PropertyManager {
         return valid;
     }
 
-    private boolean hasValidBuildingsForHotel(PropertyModel property) {
+    private boolean hasValidBuildingsForHotel() {
         return property.getHouseNumber() == MAX_NUMBER_OF_HOUSES && property.getHotelNumber() < MAX_NUMBER_OF_HOTEL;
     }
 
-    private boolean hasValidBuildingsForRemovingHotel(PropertyModel property) {
+    private boolean hasValidBuildingsForRemovingHotel() {
         return property.getHouseNumber() == 0 && property.getHotelNumber() > 0;
     }
 }
--- src/test/java/ut/PropertyManagerTest.java
+++ src/test/java/ut/PropertyManagerTest.java
@@ -10,15 +10,105 @@ import model.property.PropertyModel;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PropertyManagerTest {
-    PropertyOwnerMapper ownerMapper = TestUtils.getPropertyOwnerMapper();
-    PropertyCategoryMapper categoryMapper = TestUtils.getPropertyCategoryMapper();
+    private static final PropertyOwnerMapper ownerMapper = TestUtils.getPropertyOwnerMapper();
+    private static final PropertyCategoryMapper categoryMapper = TestUtils.getPropertyCategoryMapper();
 
     @Test
-    public void propertyManagerBuildingTest() {
-        PropertyManager propertyManager = new PropertyManager(
+    public void propertyManagerBuildingTestNotImprovable() {
+        //RAILROADS
+        final List<PropertyModel> properties = categoryMapper.getCategoryProperties(PropertyCategory.RAILROAD);
+        PropertyManager propertyManager = getPropertyManager(properties.get(0));
+        for (PropertyModel property : properties) {
+            ownerMapper.removeOwner(property);
+        }
+        Assert.assertFalse(propertyManager.buildHouse());
+
+        PlayerModel player = getPlayer();
+        for (PropertyModel property : properties) {
+            ownerMapper.setOwner(property, player);
+        }
+        Assert.assertFalse(propertyManager.canImproveHouse());
+        Assert.assertFalse(propertyManager.canImproveHotel());
+    }
+
+    @Test
+    public void propertyManagerBuildingTestPink() {
+        //PINK
+        List<PropertyModel> properties = categoryMapper.getCategoryProperties(PropertyCategory.PINK);
+        PropertyManager propertyManager = getPropertyManager(properties.get(0));
+        PlayerModel player = getPlayer();
+
+        for (PropertyModel property : properties) {
+            ownerMapper.removeOwner(property);
+        }
+        Assert.assertFalse(propertyManager.buildHouse());
+
+        for (PropertyModel property : properties) {
+            ownerMapper.setOwner(property, player);
+        }
+        Assert.assertTrue(propertyManager.buildHouse());
+        Assert.assertFalse(propertyManager.buildHouse());
+        Assert.assertFalse(propertyManager.buildHotel());
+        Assert.assertFalse(propertyManager.canSell());
+        Assert.assertFalse(propertyManager.canMortgage());
+
+        for (PropertyModel property : properties) {
+            property.setHouseNumber(0);
+            property.setHotelNumber(0);
+        }
+        Assert.assertTrue(propertyManager.canSell());
+        Assert.assertTrue(propertyManager.canCollectRent());
+        Assert.assertTrue(propertyManager.canMortgage());
+        Assert.assertTrue(propertyManager.mortgage());
+        Assert.assertFalse(propertyManager.canCollectRent());
+        Assert.assertFalse(propertyManager.buildHouse());
+        Assert.assertFalse(propertyManager.buildHotel());
+        Assert.assertFalse(propertyManager.canMortgage());
+        Assert.assertTrue(propertyManager.canLiftMortgage());
+        Assert.assertTrue(propertyManager.liftMortgage());
+
+        List<PropertyManager> managers = new ArrayList<>();
+        for (PropertyModel property : properties) {
+            managers.add(getPropertyManager(property));
+        }
+
+        do {
+            for (PropertyManager manager : managers) {
+                Assert.assertTrue(manager.buildHouse());
+            }
+        } while (managers.get(managers.size() - 1).canImproveHouse());
+        Assert.assertFalse(propertyManager.buildHouse());
+        Assert.assertFalse(propertyManager.canImproveHouse());
+        Assert.assertTrue(propertyManager.canImproveHotel());
+        Assert.assertTrue(propertyManager.buildHotel());
+        Assert.assertFalse(propertyManager.canImproveHotel());
+        Assert.assertTrue(managers.get(1).buildHotel());
+        Assert.assertTrue(propertyManager.canRemoveHotel());
+        Assert.assertTrue(propertyManager.removeHotel());
+        Assert.assertTrue(managers.get(1).removeHotel());
+        Assert.assertTrue(propertyManager.canRemoveHouse());
+
+        do {
+            for (PropertyManager manager : managers) {
+                Assert.assertTrue(manager.removeHouse());
+                if (manager != managers.get(properties.size() - 1)) {
+                    Assert.assertFalse(manager.removeHouse());
+                }
+            }
+        } while (managers.get(properties.size() - 1).canRemoveHouse());
+        for (PropertyModel property : properties) {
+            Assert.assertEquals(0, property.getHouseNumber());
+            Assert.assertEquals(0, property.getHotelNumber());
+        }
+    }
+
+    public static PropertyManager getPropertyManager(PropertyModel propertyModel) {
+        return new PropertyManager(
+                propertyModel,
                 ownerMapper,
                 categoryMapper,
                 new PriceManager(ownerMapper, categoryMapper) {
@@ -33,82 +123,9 @@ public class PropertyManagerTest {
                     }
                 }
         );
+    }
 
-        //RAILROADS
-        List<PropertyModel> properties = categoryMapper.getCategoryProperties(PropertyCategory.RAILROAD);
-        PropertyModel firstProperty = properties.get(0);
-        for (PropertyModel property : properties) {
-            ownerMapper.removeOwner(property);
-        }
-        Assert.assertFalse(propertyManager.buildHouse(firstProperty));
-
-        PlayerModel player = new PlayerModel("id", "name", 1000);
-        for (PropertyModel property : properties) {
-            ownerMapper.setOwner(property, player);
-        }
-        Assert.assertFalse(propertyManager.canImproveHouse(firstProperty));
-        Assert.assertFalse(propertyManager.canImproveHotel(firstProperty));
-
-        //PINK
-        properties = categoryMapper.getCategoryProperties(PropertyCategory.PINK);
-        firstProperty = properties.get(0);
-
-        for (PropertyModel property : properties) {
-            ownerMapper.removeOwner(property);
-        }
-        Assert.assertFalse(propertyManager.buildHouse(firstProperty));
-
-        for (PropertyModel property : properties) {
-            ownerMapper.setOwner(property, player);
-        }
-        Assert.assertTrue(propertyManager.buildHouse(firstProperty));
-        Assert.assertFalse(propertyManager.buildHouse(firstProperty));
-        Assert.assertFalse(propertyManager.buildHotel(firstProperty));
-        Assert.assertFalse(propertyManager.canSell(firstProperty));
-        Assert.assertFalse(propertyManager.canMortgage(firstProperty));
-
-        for (PropertyModel property : properties) {
-            property.setHouseNumber(0);
-            property.setHotelNumber(0);
-        }
-        Assert.assertTrue(propertyManager.canSell(firstProperty));
-        Assert.assertTrue(propertyManager.canCollectRent(firstProperty));
-        Assert.assertTrue(propertyManager.canMortgage(firstProperty));
-        Assert.assertTrue(propertyManager.mortgage(firstProperty));
-        Assert.assertFalse(propertyManager.canCollectRent(firstProperty));
-        Assert.assertFalse(propertyManager.buildHouse(firstProperty));
-        Assert.assertFalse(propertyManager.buildHotel(firstProperty));
-        Assert.assertFalse(propertyManager.canMortgage(firstProperty));
-        Assert.assertTrue(propertyManager.canLiftMortgage(firstProperty));
-        Assert.assertTrue(propertyManager.liftMortgage(firstProperty));
-
-        do {
-            for (PropertyModel property : properties) {
-                Assert.assertTrue(propertyManager.buildHouse(property));
-            }
-        } while (propertyManager.canImproveHouse(properties.get(properties.size() - 1)));
-        Assert.assertFalse(propertyManager.buildHouse(firstProperty));
-        Assert.assertFalse(propertyManager.canImproveHouse(firstProperty));
-        Assert.assertTrue(propertyManager.canImproveHotel(firstProperty));
-        Assert.assertTrue(propertyManager.buildHotel(firstProperty));
-        Assert.assertFalse(propertyManager.canImproveHotel(firstProperty));
-        Assert.assertTrue(propertyManager.buildHotel(properties.get(1)));
-        Assert.assertTrue(propertyManager.canRemoveHotel(firstProperty));
-        Assert.assertTrue(propertyManager.removeHotel(firstProperty));
-        Assert.assertTrue(propertyManager.removeHotel(properties.get(1)));
-        Assert.assertTrue(propertyManager.canRemoveHouse(firstProperty));
-
-        do {
-            for (PropertyModel property : properties) {
-                Assert.assertTrue(propertyManager.removeHouse(property));
-                if (property != properties.get(properties.size() - 1)) {
-                    Assert.assertFalse(propertyManager.removeHouse(property));
-                }
-            }
-        } while (propertyManager.canRemoveHouse(properties.get(properties.size() - 1)));
-        for (PropertyModel property : properties) {
-            Assert.assertEquals(0, property.getHouseNumber());
-            Assert.assertEquals(0, property.getHotelNumber());
-        }
+    public static PlayerModel getPlayer() {
+        return new PlayerModel("id", "name", 1000);
     }
 }
