7d27c2f0d3b3549522a11b6cff5b31cbfeba974a
CPozzoli7 <c.pozzoli7@campus.unimib.it>
2022-01-13 22:15:33 +0100

    First PlayerManager implementation

--- src/main/java/manager/player/PlayerManager.java
+++ src/main/java/manager/player/PlayerManager.java
@@ -1,4 +1,71 @@
 package manager.player;
 
+import model.player.PlayerModel;
+import model.player.PlayerState;
+import model.player.Position;
+
 public class PlayerManager {
+    private final PlayerModel player;
+    private int funds;
+    private PlayerState state;
+    private Position position;
+
+    public PlayerManager(PlayerModel player, int funds) {
+        this.player = player;
+        this.funds = funds;
+        this.state = PlayerState.FREE;
+        this.position = new Position();
+    }
+
+    public void earn(int money) {
+        funds += money;
+    }
+
+    public void spend(int money) {
+        funds -= money;
+        checkBankrupt();
+    }
+
+    public Position move(int diceRoll, boolean direct) {
+        if (canMove()) {
+            position.setPosition(position.getIntPosition() + diceRoll, direct);
+        }
+        return position;
+    }
+
+    public void goToJail() {
+        state = PlayerState.IN_JAIL;
+    }
+
+    public void getOutOfJail() {
+        state = PlayerState.FREE;
+    }
+
+    public void getOutOfJailWithFine() {
+        state = PlayerState.FINED;
+    }
+
+    public boolean canMove() {
+        PlayerState playerState = state;
+        return !PlayerState.BANKRUPT.equals(playerState) && !PlayerState.IN_JAIL.equals(playerState);
+    }
+
+    public boolean canTakeTurn() {
+        PlayerState playerState = state;
+        return !PlayerState.BANKRUPT.equals(playerState);
+    }
+
+    public boolean isInJail() {
+        return PlayerState.IN_JAIL.equals(state);
+    }
+
+    //PRIVATE METHODS
+
+    private void checkBankrupt() {
+        if (funds <= 0) {
+            state = PlayerState.BANKRUPT;
+        } else if (PlayerState.BANKRUPT.equals(state)) {
+            state = PlayerState.FREE;
+        }
+    }
 }
--- src/main/java/model/player/PlayerModel.java
+++ src/main/java/model/player/PlayerModel.java
@@ -3,15 +3,10 @@ package model.player;
 public class PlayerModel {
     private String id;
     private String name;
-    private int funds;
-    private PlayerState state;
-    private Position position;
 
     public PlayerModel(String id, String name, int funds) {
         this.id = id;
         this.name = name;
-        this.funds = funds;
-        this.state = PlayerState.FREE;
     }
 
     public String getId() {
@@ -20,33 +15,5 @@ public class PlayerModel {
 
     public String getName() {
         return name;
-    }
-
-    public int getFunds() {
-        return funds;
-    }
-
-    public PlayerState getState() {
-        return state;
-    }
-
-    public Position getPosition() {
-        return position;
-    }
-
-    public void setFunds(int funds) {
-        this.funds = funds;
-    }
-
-    public void setState(PlayerState state) {
-        this.state = state;
-    }
-
-    public void setPosition(int position) {
-        this.position.setPosition(position);
-    }
-
-    public void setDirectPosition(int position) {
-        this.position.setPosition(position, true);
     }
 }
