184bdce98eea3f095a8f26355c17d60ef61b6fea
CPozzoli7 <c.pozzoli7@campus.unimib.it>
2022-01-14 18:10:31 +0100

    First implementation of trade and player controller

--- src/main/java/controller/player/PlayerController.java
+++ src/main/java/controller/player/PlayerController.java
@@ -1,4 +1,35 @@
 package controller.player;
 
+import manager.player.PlayerManager;
+import model.PropertyOwnerMapper;
+import model.player.PlayerModel;
+
+import java.util.List;
+import java.util.Map;
+
 public class PlayerController {
+    private List<PlayerModel> players;
+    private Map<PlayerModel, PlayerManager> playerManagerMap;
+
+    public PlayerController(List<PlayerModel> players, PropertyOwnerMapper ownerMapper) {
+        this.players = players;
+        for (PlayerModel player : players) {
+            addPlayer(player, 1000, ownerMapper); //TODO Connect to game configuration
+        }
+    }
+
+    public boolean addPlayer(PlayerModel playerModel, int funds, PropertyOwnerMapper ownerMapper) {
+        players.add(playerModel);
+        playerManagerMap.put(playerModel, new PlayerManager(playerModel, funds, ownerMapper));
+        return true;
+    }
+
+    public void removePlayer(PlayerModel player) {
+        players.remove(player);
+        playerManagerMap.remove(player);
+    }
+
+    public PlayerManager getManager(PlayerModel player) {
+        return playerManagerMap.get(player);
+    }
 }
