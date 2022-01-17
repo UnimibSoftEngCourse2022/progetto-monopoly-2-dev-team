42e10e5bb6b26bc19e7356c527457d4a8767bf9d
Emanuele Papa <e.papa6@campus.unimib.it>
2022-01-17 15:33:47 +0100

    Added Getters for DrawableCards's attributes

--- src/main/java/model/DrawableCardModel.java
+++ src/main/java/model/DrawableCardModel.java
@@ -7,23 +7,23 @@ public class DrawableCardModel {
     private String text;
     private DrawableCardCategory category;
 
+    // movement commands
     private Movement movement;
     private int where;
     private boolean direct;
 
+    // pay commands
     private Pay pay;
     private int amount;
     private int houseFee;
     private int hotelFee;
 
+    // payments commands
     private Creditor creditor;
     private Debtor debtor;
 
+    // out of jail free commands
     private boolean keep;
-
-
-
-
 
 
     public String getText() {
@@ -33,6 +33,44 @@ public class DrawableCardModel {
     public DrawableCardCategory getCategory() {
         return category;
     }
-    
 
+    public Movement getMovement() {
+        return movement;
+    }
+
+    public int getWhere() {
+        return where;
+    }
+
+    public boolean isDirect() {
+        return direct;
+    }
+
+    public Pay getPay() {
+        return pay;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public int getHouseFee() {
+        return houseFee;
+    }
+
+    public int getHotelFee() {
+        return hotelFee;
+    }
+
+    public Creditor getCreditor() {
+        return creditor;
+    }
+
+    public Debtor getDebtor() {
+        return debtor;
+    }
+
+    public boolean isKeep() {
+        return keep;
+    }
 }
