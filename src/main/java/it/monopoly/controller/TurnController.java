6d6b9b90874d041adaed61351ee529bc02cdbaf8
CPozzoli7 <c.pozzoli7@campus.unimib.it>
2022-01-19 19:02:48 +0100

    First turn controller logic

--- src/main/java/it/monopoly/controller/TurnController.java
+++ src/main/java/it/monopoly/controller/TurnController.java
@@ -1,4 +1,31 @@
 package it.monopoly.controller;
 
+import it.monopoly.controller.player.PlayerController;
+import it.monopoly.manager.player.PlayerManager;
+import it.monopoly.model.player.PlayerModel;
+
 public class TurnController {
+    private final PlayerController playerController;
+    private int currentPlayerIndex = 0;
+
+    public TurnController(PlayerController playerController) {
+        this.playerController = playerController;
+    }
+
+    private void nextTurn() {
+        currentPlayerIndex = ++currentPlayerIndex % playerController.getModels().size();
+        notifyPlayerTurn();
+    }
+
+    private void notifyPlayerTurn() {
+//        PlayerManager manager = playerController.getManager(getCurrentPlayer());
+//        boolean tookTurn = manager.startTurn();
+//        if(!tookTurn) {
+//            nextTurn();
+//        }
+    }
+
+    private PlayerModel getCurrentPlayer() {
+        return playerController.getModels().get(currentPlayerIndex);
+    }
 }
